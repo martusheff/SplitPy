6fbfffaa23be248db889e69b1247e38c64c382b1
Andronick Martusheff <71955336+martusheff@users.noreply.github.com>
2021-08-02 14:39:48 -0700

    Working Version

---
+++ main.py
@@ -0,0 +1,21 @@
+class Friend:
+
+    def __init__(self, name):
+        self.name = name
+
+
+
+def main():
+    # Use a breakpoint in the code line below to debug your script.
+    pool = 0
+    party_size = input("How many of y'all ain't trynna do 50 different cash-app transfers?\n")
+    switch()
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+
+
